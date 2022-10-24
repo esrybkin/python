73867bff72fab710a5c4ad7cb3e8afe65400981d
rybkin <erybkin@yandex.ru>
2022-10-24 15:21:48 +0700

    add lesson04

---
+++ Lesson04/logger.py
@@ -0,0 +1,18 @@
+from datetime import datetime as dt
+
+def temperature_logger(data):
+    time = dt.now().strftime('%H:%M')
+    with open('log.csv', 'a') as file:
+        file.write('{};temperature;{}\n'.format(time, data))
+
+
+def preassure_logger(data):
+    time = dt.now().strftime('%H:%M')
+    with open('log.csv', 'a') as file:
+        file.write('{};preassure;{}\n'.format(time, data))
+
+
+def wind_speed_logger(data):
+    time = dt.now().strftime('%H:%M')
+    with open('log.csv', 'a') as file:
+        file.write('{};wind_speed;{}\n'.format(time, data))
